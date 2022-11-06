517b10388dac6b1d6ffb0af9255721bb1408bfea
KacperPrarat <115100470+KacperPrarat@users.noreply.github.com>
2022-11-06 22:26:57 +0100

    Update zad1.py

--- zad1.py
+++ zad1.py
@@ -1,8 +1,21 @@
 A = [[1,2,3],[4,5,6]]
+listaprzejsciowa = []
+liczbakolumn = len(A)
+liczbawierszy = len(A[0])
+for element in A:
+    for j in element:
+        listaprzejsciowa.append(j)
 At = []
-kolumnawAt = 0
-for i in range(len(A[0])):
-   At.append([]*len(A[0]))
-#for kolumna in range(len(A)):
-print(At)
-
+for i in range(liczbawierszy):
+    At.append([] * liczbawierszy)
+licznik = 0
+liczba = 0
+for element in At:
+    liczba = 0
+    dodatek = 0
+    for i in range(liczbakolumn):
+        element.append(listaprzejsciowa[liczba + licznik])
+        liczba = liczba + liczbawierszy
+    licznik = licznik + 1
+for l in At:
+    print(l)
